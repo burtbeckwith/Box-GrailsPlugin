8ec4b996f071b20d52fabb3af27f2f774ee5f037
Burt Beckwith <burt@burtbeckwith.com>
2014-09-23 15:54:15 -0400

    cleanup

--- BoxGrailsPluginGrailsPlugin.groovy
+++ BoxGrailsPluginGrailsPlugin.groovy
@@ -1,12 +1,12 @@
 class BoxGrailsPluginGrailsPlugin {
     def version = '1.2'
     def grailsVersion = '2.0 > *'
-    def title = 'Box Plugin for Grails'
-    def description = 'Adds integration with the Box API. Here with this plugin you can use all Box services within from your Grails application'
+    def title = 'Box.com Plugin for Grails'
+    def description = 'Adds integration with the Box.com API. Here with this plugin you can use all Box.com services within from your Grails application'
     def documentation = 'https://github.com/hitenpratap/Box-GrailsPlugin/wiki/Box-for-Grails'
     def license = 'APACHE'
     def developers = [
-            [name: 'Hiten Pratap Singh', email: 'hitenpratap99@gmail.com',github:'https://github.com/hitenpratap']
+        [name: 'Hiten Pratap Singh', email: 'hitenpratap99@gmail.com',github:'https://github.com/hitenpratap']
     ]
     def issueManagement = [system: 'GITHUB', url: 'https://github.com/hitenpratap/Box-GrailsPlugin/issues']
     def scm = [url: 'https://github.com/hitenpratap/Box-GrailsPlugin']
